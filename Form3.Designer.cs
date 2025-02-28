4eb32e42145a1adc943d2ac18857441da57830b5
FZoltan40 <farkasz@kkszki.hu>
2025-02-28 13:34:40 +0100

    Category and Post can be addded.

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -29,30 +29,92 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(231, 12);
+            this.listBox1.Location = new System.Drawing.Point(238, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(300, 355);
+            this.listBox1.Size = new System.Drawing.Size(300, 316);
             this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(238, 418);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.MouseEnter += new System.EventHandler(this.textBox1_MouseEnter);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(238, 348);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 60);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(483, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = " <-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(238, 327);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(300, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(642, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 124);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form3";
-            this.Text = "Form3";
+            this.Text = "Kommentek";
+            this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -1,6 +1,10 @@
 ﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using static BlogDesktop.Form1;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace BlogDesktop
 {
@@ -10,6 +14,7 @@ namespace BlogDesktop
         {
             InitializeComponent();
             ListComments();
+            ListCategory();
         }
         private const string ConnectionString = "Server=localhost;Database=blog;Uid=root;Password=;SslMode=None";
         private bool ListComments()
@@ -30,8 +35,7 @@ namespace BlogDesktop
                         var comment = new
                         {
                             Comment = dr.GetString(0),
-                            User = dr.GetString(1),
-                            Id = UserId.Id
+                            User = dr.GetString(1)
                         }; 
                         listBox1.Items.Add(comment);
                         
@@ -44,6 +48,133 @@ namespace BlogDesktop
             catch
             {
                 return false;
+            }
+        }
+
+        private bool ListCategory()
+        {
+            try
+            {
+                using (var connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    string sql =
+                    "SELECT `CategoryName` FROM `category` ";
+
+                    MySqlCommand cmd = new MySqlCommand(sql, connection);
+
+                    MySqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        var category = new
+                        {
+                            Name = dr.GetString(0),
+                        };
+                        comboBox1.Items.Add(category.Name);
+
+                    }
+                    connection.Close();
+
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private string AddNewPost(string title, string post)
+        {
+            try
+            {
+                string sql = "INSERT INTO `blogtable`(`Title`, `Post`, `UserId`) VALUES (@title,@post,@userid)";
+
+                using (var connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+
+                    using (var command = new MySqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@title", title);
+                        command.Parameters.AddWithValue("@post", post);
+                        command.Parameters.AddWithValue("@userid", UserId.Id);
+
+                        command.ExecuteNonQuery();
+                    }
+
+                    connection.Close();
+                    listBox1.Items.Clear();
+                    ListComments();
+                    return "Sikeres post hozzáadás.";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        private string AddNewCategory(string category)
+        {
+            try
+            {
+                string sql = "INSERT INTO `category` (`CategoryName`) VALUES (@category)";
+
+                using (var connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+
+                    using (var command = new MySqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@category", category);
+
+                        command.ExecuteNonQuery();
+                    }
+
+                    connection.Close();
+                    comboBox1.Items.Clear();
+                    ListCategory();
+                    return "Sikeres post hozzáadás.";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            AddNewPost(textBox1.Text, textBox2.Text);
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+               MessageBox.Show(AddNewCategory(textBox1.Text));
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
             }
         }
     }
